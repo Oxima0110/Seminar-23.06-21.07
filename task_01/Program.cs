c32d34ffcc91e2ac2a63e9f64950200e77b5bd8c
Oxana <crazyglass@list.ru>
2022-06-24 12:04:49 +0300

    Задача 03

---
+++ task_01/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a == b*b)
+{
+  Console.WriteLine("да");  
+}
+else
+{
+   Console.WriteLine("нет"); 
+}
