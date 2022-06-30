da698eb38d59b1759e0eb16d3b4de1957b8cd965
Oxana <crazyglass@list.ru>
2022-06-30 22:48:44 +0300

    Исправление switch-case

--- task_18/Program.cs
+++ task_18/Program.cs
@@ -2,7 +2,7 @@
 // заданному номеру четверти, показывает диапазон
 // возможных координат точек в этой четверти (x и y).
 
- Console.WriteLine("Введите номер четверти от 1 до 4");
+Console.WriteLine("Введите номер четверти от 1 до 4");
 int x = Convert.ToInt32(Console.ReadLine());
 switch (x)
 {
@@ -12,3 +12,12 @@ switch (x)
     case 4: Console.WriteLine("x > 0 и y < 0"); break;
     default: Console.WriteLine("Нет такой четверти"); break;
 }
+
+// string Quarter(int num)
+// {
+//     case 1: return "x > 0 && y > 0"; 
+//     case 2: return "x < 0 && y > 0"; 
+//     case 3: return "x < 0 && y < 0"; 
+//     case 4: return "x > 0 &&  y < 0";
+// return "Нет такой четверти";
+// }
